d8c5f77375ff2e9b6fffa381a812604e535040fe
FARDIN98 <fardinahmed66@gmail.com>
2021-03-10 00:25:02 +0600

    finished

--- src/Component/Friend/Friend.js
+++ src/Component/Friend/Friend.js
@@ -1,9 +1,16 @@
+
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Friend.css'
+
 
 const Friend = (props) => {
     const{name,email,phone,website,id} = props.friend
+    const history = useHistory()
+    const handleClick = (id) => {
+        history.push(`/friend/${id}`)
+    }
     return (
         <div className = "friendStyle">
             <div className = "friendStyleInside">
@@ -11,7 +18,9 @@ const Friend = (props) => {
                 <p><b>Email: {email}</b></p>
                 <p><b>Phone: {phone}</b></p>
                 <p><b>Website: {website}</b></p>
-                <button><Link to={`/friend/${id}`}>Show Detail of {id}</Link></button>
+                <Link to={`/friend/${id}`}>
+                    <button onClick={() => handleClick(id)}>Click me</button>
+                </Link>
             </div>
             
         </div>
--- src/Component/FriendDetail/FriendDetail.js
+++ src/Component/FriendDetail/FriendDetail.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-
+import './FriendDetail.css'
 const FriendDetail = () => {
     const{id} = useParams()
     const [friend,setFriend] = useState({})
-    // const {name,email,phone,website,address} = friend
+    
     useEffect(()=>{
         fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then(res => res.json())
         .then(data => setFriend(data))
     }, [])
     return (
-        <div>
+        <div className="friendDetailStyle">
             <h3>This is friend detail {id}</h3>
-            {/* <p><b>Name: {name}</b></p> */}
-            <p><b>Address: {friend.address.street}</b></p>
+            <p><b>Name: {friend.name}</b></p>
+            <p><b>Email: {friend.email}</b></p>
+            <p><b>Phone: {friend.phone}</b></p>
+            <p><b>Website: {friend.website}</b></p>
+            <p><b>Address: {friend.address?.city + "," + friend.address?.street + "," + friend.address?.zipcode}</b></p>  
         </div>
     );
 };
